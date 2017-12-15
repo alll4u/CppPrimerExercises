ffb4376b9db3dd9d7b231fde3c49132d78df6f26
alll4u <alll4u@github.com>
2017-12-15 17:31:39 +0800

    12-15

--- CppPrimerExercises/main.cpp
+++ CppPrimerExercises/main.cpp
@@ -4,6 +4,7 @@
 #include <forward_list>
 #include <fstream>
 #include <string>
+#include <map>
 /*
  * @ No      :9.26
  * @ brief   :单迭代器erase删除list元素
@@ -14,60 +15,60 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int ia[]={0,2,2,2,1,1,2,3,5,8,13,21,55,89};
-    vector<int> vec(ia, end(ia));
-    list<int> lst(ia, end(ia));
-    forward_list<int> flst(ia, end(ia));
-//    for(int i=0;i<11;i++){
-//        vec.push_back(ia[i]);
-//        lst.push_back(ia[i]);
+//    int ia[]={0,2,2,2,1,1,2,3,5,8,13,21,55,89};
+//    vector<int> vec(ia, end(ia));
+//    list<int> lst(ia, end(ia));
+//    forward_list<int> flst(ia, end(ia));
+////    for(int i=0;i<11;i++){
+////        vec.push_back(ia[i]);
+////        lst.push_back(ia[i]);
+////    }
+//    for(auto iter=vec.begin();iter<vec.end();)
+//    {
+//        if(*iter%2==0){
+//            iter=vec.erase(iter);
+//        }
+//        else{
+//            iter++;
+//        }
 //    }
-    for(auto iter=vec.begin();iter<vec.end();)
-    {
-        if(*iter%2==0){
-            iter=vec.erase(iter);
-        }
-        else{
-            iter++;
-        }
-    }
-    for(auto iter=lst.begin();iter!=lst.end();)
-    {
-        if(*iter%2){
-            iter=lst.erase(iter);
-        }
-        else {
-            iter++;
-        }
-    }
-    for(auto e:lst){
-        cout << e << endl;
-    }
-    vector<int> v = {1,2,3,4};
-    auto begin = v.begin();
-    while(begin != v.end()){
-//        ++begin;
-        begin = v.insert(begin, 42);
-        begin+=2;
-    }
-    for(auto e:v){
-        cout << e << endl;
-    }
-    ofstream fs;
-    fs.open("/Users/fei/results.txt");
-    vector<int> a;
+//    for(auto iter=lst.begin();iter!=lst.end();)
+//    {
+//        if(*iter%2){
+//            iter=lst.erase(iter);
+//        }
+//        else {
+//            iter++;
+//        }
+//    }
+//    for(auto e:lst){
+//        cout << e << endl;
+//    }
+//    vector<int> v = {1,2,3,4};
+//    auto begin = v.begin();
+//    while(begin != v.end()){
+////        ++begin;
+//        begin = v.insert(begin, 42);
+//        begin+=2;
+//    }
+//    for(auto e:v){
+//        cout << e << endl;
+//    }
+//    ofstream fs;
+//    fs.open("/Users/fei/results.txt");
+//    vector<int> a;
 
-    for(int i;i<10000;i++){
-        a.push_back(i);
-        fs << "a.size:" << a.size() <<" a.capacity:" << a.capacity() << endl;
-    }
-    fs.close();
+//    for(int i;i<10000;i++){
+//        a.push_back(i);
+//        fs << "a.size:" << a.size() <<" a.capacity:" << a.capacity() << endl;
+//    }
+//    fs.close();
 
-    string str = "abcde through thru";
-    string str2 = string{str.begin(), str.begin()+3};
-    cout << str2 << endl;
-    string oldVal = "thru";
-    string newVal = "through";
+//    string str = "abcde through thru";
+//    string str2 = string{str.begin(), str.begin()+3};
+//    cout << str2 << endl;
+//    string oldVal = "thru";
+//    string newVal = "through";
 //    for(auto beg=str.begin();distance(beg, str.end())>=distance(oldVal.begin(), oldVal.end());){
 //        if(string{beg, beg+oldVal.size()} == oldVal){
 //            beg = str.erase(beg, beg+oldVal.size());
@@ -98,15 +99,28 @@ int main(int argc, char *argv[])
 //                longest_str=word;
 //    }
 //    cout << longest_str;
-    string test = "ab2c4d7c4e6";
-    string number{"1234567890"};
-    string character = "abcdefg";
-    vector<string> results;
-    cout << "numeric characters:"<<endl;
-    for(string::size_type pos=0;pos!=string::npos;++pos){
-        pos = pos=test.find_first_of(number,pos);
-        cout << "position: "<<pos<<endl;
-        cout << test[pos] << " ";
+//    string test = "ab2c4d7c4e6";
+//    string number{"1234567890"};
+//    string character = "abcdefg";
+//    vector<string> results;
+//    cout << "numeric characters:"<<endl;
+//    for(string::size_type pos=0;pos!=string::npos;++pos){
+//        pos = pos=test.find_first_of(number,pos);
+//        cout << "position: "<<pos<<endl;
+//        cout << test[pos] << " ";
+//    }
+
+    multimap<string, string> example={{"aaa", "bbb"},{"ddd","d"}};
+    multimap<string, string> family;
+    family.insert(example.begin(), example.end());
+    for(auto e:family){
+        cout << e.first << ":" << e.second<<endl;
     }
+    map<int, string> m = {{1,"ss"}, {2, "sz"}};
+    using KeyType = std::map<int, std::string>::key_type;
+    cout << "type to subscript: "<<typeid(KeyType).name() << endl;
+    cout << "returned from the subscript operator: "<<typeid(decltype(m[1])).name() << endl;
+
+
     return 0;
 }
